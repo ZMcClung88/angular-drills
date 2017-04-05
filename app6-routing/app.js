bfd0c4f6e7c4dd93d450cdf9993eeb0734897e81
Zac McClung <zac.mcclung370@gmail.com>
2017-04-05 08:21:49 -0600

    needs finished

---
+++ app6-routing/app.js
@@ -0,0 +1,18 @@
+angular.module('myApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider
+        .otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: './index.html'
+    })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: './signup.html'
+    })
+    .state('details', {
+      url: '/details',
+      templateUrl: './details.html'
+    })
+})
