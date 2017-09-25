c6b6aaa482034c647c37cb51fbd917640dac3311
Zac McClung <zac.mcclung370@gmail.com>
2017-09-25 01:00:54 -0600

    completed project

---
+++ app9-directive-select-all/directive.js
@@ -0,0 +1,14 @@
+angular.module('app9').directive('highlightThis', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      element.on('mousedown', function() {
+        var text = element.val();
+        this.select();
+      })
+      element.on('mouseup', function(e) {
+        e.preventDefault();
+      })
+    }
+  }
+})
